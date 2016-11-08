66d38fecb3bc4d74a30751608039fe1b03ad5285
ismaelmedinarodriguez <ismaelmedinarodriguez@172.19.241.210>
2016-11-08 16:54:52 +0100

    inicializacion

---
+++ multas/src/main/java/edu/uclm/esi/iso2/multas/App.java
@@ -0,0 +1,13 @@
+package edu.uclm.esi.iso2.multas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ multas/src/main/java/edu/uclm/esi/iso2/multas/dao/DriverDao.java
@@ -0,0 +1,29 @@
+package edu.uclm.esi.iso2.multas.dao;
+
+import javax.persistence.Query;
+
+import org.hibernate.HibernateException;
+
+import edu.uclm.esi.iso2.multas.domain.Driver;
+
+public class DriverDao extends GeneralDao<Driver> {
+	public DriverDao() {
+		super();
+	}
+	
+	public Driver findByDni(String dni) throws HibernateException {
+		Driver driver = null;
+        try {
+            startOperation();
+            Query query=session.createQuery("from Driver where dni=?");
+            query.setParameter(0, dni);
+            driver = (Driver) query.getSingleResult();
+            transaction.commit();
+        } catch (HibernateException e) {
+            throw e;
+        } finally {
+            HibernateFactory.close(session);
+        }
+        return driver;
+	}
+}
