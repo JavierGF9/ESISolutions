ee5741365433f2d76126c05d256deb53a1e5d272
ismaelmedinarodriguez <ismaelmedinarodriguez@172.19.144.181>
2016-12-07 12:49:41 +0100

    hibernate funcionando

--- multas/src/main/java/edu/uclm/esi/iso2/multas/App.java
+++ multas/src/main/java/edu/uclm/esi/iso2/multas/App.java
@@ -14,6 +14,7 @@ public class App {
 	public static void main(String[] args) {
 		
 		session=HibernateUtils.getSessionFactory().openSession();
+		System.out.println("Hibernateeeeeeeeeeeeeee");
 		
 		
 		
