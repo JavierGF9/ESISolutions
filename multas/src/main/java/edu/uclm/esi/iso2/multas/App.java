04b65dd1b07449af9cc5092658befcf591d43774
ismaelmedinarodriguez <ismaelmedinarodriguez@172.19.144.181>
2016-12-07 19:04:32 +0100

    Cambiado el pom

--- multas/src/main/java/edu/uclm/esi/iso2/multas/App.java
+++ multas/src/main/java/edu/uclm/esi/iso2/multas/App.java
@@ -2,6 +2,7 @@ package edu.uclm.esi.iso2.multas;
 
 import org.hibernate.Session;
 
+import edu.uclm.esi.iso2.multas.domain.Driver;
 import edu.uclm.esi.iso2.multas.persistencia.HibernateUtils;
 
 /**
@@ -14,7 +15,9 @@ public class App {
 	public static void main(String[] args) {
 		
 		session=HibernateUtils.getSessionFactory().openSession();
-		System.out.println("Hibernateeeeeeeeeeeeeee");
+		
+		Driver contact = (Driver) session.get(Driver.class, new Integer(3));
+		System.out.println(contact.getName());
 		
 		
 		
