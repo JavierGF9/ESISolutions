2e05754e67dd2eac11180899032bda8f097a6690
ismaelmedinarodriguez <ismaelmedinarodriguez@172.19.144.181>
2016-12-07 23:37:30 +0100

    Arreglado error del log

--- multas/src/main/java/edu/uclm/esi/iso2/multas/App.java
+++ multas/src/main/java/edu/uclm/esi/iso2/multas/App.java
@@ -2,6 +2,8 @@ package edu.uclm.esi.iso2.multas;
 
 
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.status.StatusLogger;
 import org.hibernate.Session;
 
 import edu.uclm.esi.iso2.multas.dao.HibernateFactory;
@@ -12,16 +14,20 @@ import edu.uclm.esi.iso2.multas.domain.Driver;
  * Hello world!
  */
 public class App {
+	//Para evitar que salgo el error en el log
+	static {
+	    StatusLogger.getLogger().setLevel(Level.OFF);
+	}
 
 	public static void main(String[] args) {
-
+		
 		
 
 		HibernateFactory.buildIfNeeded();
 		Session sessFact = HibernateFactory.getSessionFactory().openSession();
 		org.hibernate.Transaction tr = sessFact.beginTransaction();
-		Driver driver = (Driver) sessFact.get(Driver.class, new Integer(10));
-		System.out.println("pureba"+driver.getDni()+" "+driver.getName());
+		Driver driver = (Driver) sessFact.get(Driver.class, new Integer(200));
+		System.out.println("pureba "+driver.getDni()+" ");
 
 		sessFact.close();
 	}
