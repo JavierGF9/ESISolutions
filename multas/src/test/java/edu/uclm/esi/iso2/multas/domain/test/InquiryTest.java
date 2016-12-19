d5d419832cecc781d3ef5a5d3ddc8c8671c6d62d
Javier Gómez Fernández <javigofe@gmail.com>
2016-12-19 18:25:06 +0100

    Realizado los tests del Inquiry. Ahora deberían funcionar bien con todo

--- multas/src/test/java/edu/uclm/esi/iso2/multas/domain/test/InquiryTest.java
+++ multas/src/test/java/edu/uclm/esi/iso2/multas/domain/test/InquiryTest.java
@@ -1,7 +1,6 @@
 package edu.uclm.esi.iso2.multas.domain.test;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.status.StatusLogger;
@@ -16,18 +15,17 @@ import edu.uclm.esi.iso2.multas.radar.Carretera;
 public class InquiryTest {
 
 	@Test
-	public void testPuntos30() {
+	public void testAmount30() {
 		StatusLogger.getLogger().setLevel(Level.OFF);
 		HibernateFactory.buildIfNeeded();
 		Session sessFact = HibernateFactory.getSessionFactory().openSession();
 
-		Inquiry i = new Inquiry("0100", 70.0, Carretera.URBANA.name(), 30.0);
+		Inquiry i = new Inquiry("0100", 100.0, Carretera.URBANA.name(), 30.0);
 		Sanction s = i.createSanctionFor(i.getOwner().getDni());
 		
 		sessFact.close();
 		
 		assertTrue(s.getAmount() == 600);
-		fail("La cantidad no es la que debería ser");
 	}
 
 	@Test
@@ -40,305 +38,22 @@ public class InquiryTest {
 		Sanction s = i.createSanctionFor(i.getOwner().getDni());
 		
 		sessFact.close();
+		
+		assertTrue(s.getPoints() == 0);
 	}
 
 	@Test
-	public void testPuntos60() {
+	public void testAmount50() {
 
 		StatusLogger.getLogger().setLevel(Level.OFF);
 		HibernateFactory.buildIfNeeded();
 		Session sessFact = HibernateFactory.getSessionFactory().openSession();
 
-		// Creacion de la clase
-		// Inquiry i= new Inquiry(String license, double speed, String location,
-		// double maxSpeed)
-
+		Inquiry i = new Inquiry("0100", 70.0, Carretera.URBANA.name(), 50.0);
+		Sanction s = i.createSanctionFor(i.getOwner().getDni());
+		
 		sessFact.close();
-
-		// Comprobar que entre en el if de velocidad maxima 60
-
-	}
-
-	@Test
-	public void testPuntos70() {
-
-		StatusLogger.getLogger().setLevel(Level.OFF);
-		HibernateFactory.buildIfNeeded();
-		Session sessFact = HibernateFactory.getSessionFactory().openSession();
-
-		// Creacion de la clase
-		// Inquiry i= new Inquiry(String license, double speed, String location,
-		// double maxSpeed)
-
-		sessFact.close();
-
-		// Comprobar que entre en el if de velocidad maxima 70
-
-	}
-
-	@Test
-	public void testPuntos80() {
-
-		StatusLogger.getLogger().setLevel(Level.OFF);
-		HibernateFactory.buildIfNeeded();
-		Session sessFact = HibernateFactory.getSessionFactory().openSession();
-
-		// Creacion de la clase
-		// Inquiry i= new Inquiry(String license, double speed, String location,
-		// double maxSpeed)
-
-		sessFact.close();
-
-		// Comprobar que entre en el if de velocidad maxima 80
-
-	}
-
-	@Test
-	public void testPuntos90() {
-
-		StatusLogger.getLogger().setLevel(Level.OFF);
-		HibernateFactory.buildIfNeeded();
-		Session sessFact = HibernateFactory.getSessionFactory().openSession();
-
-		// Creacion de la clase
-		// Inquiry i= new Inquiry(String license, double speed, String location,
-		// double maxSpeed)
-
-		sessFact.close();
-
-		// Comprobar que entre en el if de velocidad maxima 90
-
-	}
-	
-	@Test
-	public void testPuntos100() {
-
-		StatusLogger.getLogger().setLevel(Level.OFF);
-		HibernateFactory.buildIfNeeded();
-		Session sessFact = HibernateFactory.getSessionFactory().openSession();
-
-		// Creacion de la clase
-		// Inquiry i= new Inquiry(String license, double speed, String location,
-		// double maxSpeed)
-
-		sessFact.close();
-
-		// Comprobar que entre en el if de velocidad maxima 100
-
-	}
-
-	@Test
-	public void testPuntos110() {
-
-		StatusLogger.getLogger().setLevel(Level.OFF);
-		HibernateFactory.buildIfNeeded();
-		Session sessFact = HibernateFactory.getSessionFactory().openSession();
-
-		// Creacion de la clase
-		// Inquiry i= new Inquiry(String license, double speed, String location,
-		// double maxSpeed)
-
-		sessFact.close();
-
-		// Comprobar que entre en el if de velocidad maxima 110
-
-	}
-
-	@Test
-	public void testPuntos120() {
-
-		StatusLogger.getLogger().setLevel(Level.OFF);
-		HibernateFactory.buildIfNeeded();
-		Session sessFact = HibernateFactory.getSessionFactory().openSession();
-
-		// Creacion de la clase
-		// Inquiry i= new Inquiry(String license, double speed, String location,
-		// double maxSpeed)
-
-		sessFact.close();
-
-		// Comprobar que entre en el if de velocidad maxima 120
-
-	}
-
-	@Test
-	public void testCantidad30() {
-
-		StatusLogger.getLogger().setLevel(Level.OFF);
-		HibernateFactory.buildIfNeeded();
-		Session sessFact = HibernateFactory.getSessionFactory().openSession();
-
-		// Creacion de la clase
-		// Inquiry i= new Inquiry(String license, double speed, String location,
-		// double maxSpeed)
-
-		sessFact.close();
-
-		// comprobamos todos los rangos en la velocidad
-		// comprobamos cada rango con la cantidad que le corresponde
-		// AQUI DEBERIA FALLAR AL SOBREPASAR LOS 80!
-
-	}
-
-	@Test
-	public void testCantidad40() {
-
-		StatusLogger.getLogger().setLevel(Level.OFF);
-		HibernateFactory.buildIfNeeded();
-		Session sessFact = HibernateFactory.getSessionFactory().openSession();
-
-		// Creacion de la clase
-		// Inquiry i= new Inquiry(String license, double speed, String location,
-		// double maxSpeed)
-
-		sessFact.close();
-
-		// comprobamos todos los rangos en la velocidad
-		// comprobamos cada rango con la cantidad que le corresponde
-
-	}
-
-	@Test
-	public void testCantidad50() {
-
-		StatusLogger.getLogger().setLevel(Level.OFF);
-		HibernateFactory.buildIfNeeded();
-		Session sessFact = HibernateFactory.getSessionFactory().openSession();
-
-		// Creacion de la clase
-		// Inquiry i= new Inquiry(String license, double speed, String location,
-		// double maxSpeed)
-
-		sessFact.close();
-
-		// comprobamos todos los rangos en la velocidad
-		// comprobamos cada rango con la cantidad que le corresponde
-
-	}
-
-	@Test
-	public void testCantidad60() {
-
-		StatusLogger.getLogger().setLevel(Level.OFF);
-		HibernateFactory.buildIfNeeded();
-		Session sessFact = HibernateFactory.getSessionFactory().openSession();
-
-		// Creacion de la clase
-		// Inquiry i= new Inquiry(String license, double speed, String location,
-		// double maxSpeed)
-
-		sessFact.close();
-
-		// comprobamos todos los rangos en la velocidad
-		// comprobamos cada rango con la cantidad que le corresponde
-
-	}
-
-	@Test
-	public void testCantidad70() {
-
-		StatusLogger.getLogger().setLevel(Level.OFF);
-		HibernateFactory.buildIfNeeded();
-		Session sessFact = HibernateFactory.getSessionFactory().openSession();
-
-		// Creacion de la clase
-		// Inquiry i= new Inquiry(String license, double speed, String location,
-		// double maxSpeed)
-
-		sessFact.close();
-
-		// comprobamos todos los rangos en la velocidad
-		// comprobamos cada rango con la cantidad que le corresponde
-
-	}
-
-	@Test
-	public void testCantidad80() {
-
-		StatusLogger.getLogger().setLevel(Level.OFF);
-		HibernateFactory.buildIfNeeded();
-		Session sessFact = HibernateFactory.getSessionFactory().openSession();
-
-		// Creacion de la clase
-		// Inquiry i= new Inquiry(String license, double speed, String location,
-		// double maxSpeed)
-
-		sessFact.close();
-
-		// comprobamos todos los rangos en la velocidad
-		// comprobamos cada rango con la cantidad que le corresponde
-
-	}
-
-	@Test
-	public void testCantidad90() {
-
-		StatusLogger.getLogger().setLevel(Level.OFF);
-		HibernateFactory.buildIfNeeded();
-		Session sessFact = HibernateFactory.getSessionFactory().openSession();
-
-		// Creacion de la clase
-		// Inquiry i= new Inquiry(String license, double speed, String location,
-		// double maxSpeed)
-
-		sessFact.close();
-
-		// comprobamos todos los rangos en la velocidad
-		// comprobamos cada rango con la cantidad que le corresponde
-
-	}
-
-	@Test
-	public void testCantidad100() {
-
-		StatusLogger.getLogger().setLevel(Level.OFF);
-		HibernateFactory.buildIfNeeded();
-		Session sessFact = HibernateFactory.getSessionFactory().openSession();
-
-		// Creacion de la clase
-		// Inquiry i= new Inquiry(String license, double speed, String location,
-		// double maxSpeed)
-
-		sessFact.close();
-
-		// comprobamos todos los rangos en la velocidad
-		// comprobamos cada rango con la cantidad que le corresponde
-
-	}
-
-	@Test
-	public void testCantidad110() {
-
-		StatusLogger.getLogger().setLevel(Level.OFF);
-		HibernateFactory.buildIfNeeded();
-		Session sessFact = HibernateFactory.getSessionFactory().openSession();
-
-		// Creacion de la clase
-		// Inquiry i= new Inquiry(String license, double speed, String location,
-		// double maxSpeed)
-
-		sessFact.close();
-
-		// comprobamos todos los rangos en la velocidad
-		// comprobamos cada rango con la cantidad que le corresponde
-
-	}
-
-	@Test
-	public void testCantidad120() {
-
-		StatusLogger.getLogger().setLevel(Level.OFF);
-		HibernateFactory.buildIfNeeded();
-		Session sessFact = HibernateFactory.getSessionFactory().openSession();
-
-		// Creacion de la clase
-		// Inquiry i= new Inquiry(String license, double speed, String location,
-		// double maxSpeed)
-
-		sessFact.close();
-
-		// comprobamos todos los rangos en la velocidad
-		// comprobamos cada rango con la cantidad que le corresponde
-
+		
+		assertTrue(s.getAmount() == 100);
 	}
 }
