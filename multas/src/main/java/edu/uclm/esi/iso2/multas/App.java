daa9236f6d1e6e89ec6442db90f5daa105ec1033
ismaelmedinarodriguez <ismaelmedinarodriguez@172.19.180.143>
2016-12-20 13:17:23 +0100

    log4j2

--- multas/src/main/java/edu/uclm/esi/iso2/multas/App.java
+++ multas/src/main/java/edu/uclm/esi/iso2/multas/App.java
@@ -2,16 +2,11 @@ package edu.uclm.esi.iso2.multas;
 
 import edu.uclm.esi.iso2.multas.funcionario.presentation.*;
 
-import org.apache.logging.log4j.Level;
-
-import org.apache.logging.log4j.status.StatusLogger;
 
 
 public class App {
 	
-	static {
-		 	    StatusLogger.getLogger().setLevel(Level.OFF);
-		 	}
+	
 
 	public static void main(String[] args) {
 		
